7c8f4666ced2016742d1221c2a111298367512ed
George Shalvashvili <shalva258@gmail.com>
2022-11-24 22:20:24 +0400

    update

--- Package.swift
+++ Package.swift
@@ -1,37 +1,25 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let remoteKotlinUrl = "https://github.com/shalva97/google-auth-decode/releases/download/0.0.17/shared.xcframework.zip"
+let remoteKotlinChecksum = "243646153ba4a0d04034a3d58d4910098b2332ce4e8a5e4e814829280dcffef9"
+let packageName = "shared"
+
 let package = Package(
-    name: "MyLibrary",
+    name: packageName,
     platforms: [
-        .macOS(.v10_14)
+        .macos(.x86_64)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary", "SomeRemoteBinaryPackage", "GoogleAuthDecode"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
+            name: packageName,
+            targets: [packageName]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyLibrary"
-        ),
         .binaryTarget(
-            name: "SomeRemoteBinaryPackage",
-            url: "https://url/to/some/remote/xcframework.zip",
-            checksum: "The checksum of the ZIP archive that contains the XCFramework."
-        ),
-        .binaryTarget(
-            name: "GoogleAuthDecode",
-            path: "build/XCFrameworks/release/google_auth_decode.xcframework"
+            name: packageName,
+            url: remoteKotlinUrl,
         )
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
     ]
 )
