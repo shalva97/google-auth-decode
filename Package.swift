3f20ec31f880994971d01f216e57bfefe7b258ba
George Shalvashvili <shalva258@gmail.com>
2022-12-08 20:02:24 +0400

    bump version to 19

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let remoteKotlinUrl = "https://github.com/shalva97/google-auth-decode/releases/download/0.0.18/shared.xcframework.zip"
+let remoteKotlinUrl = "https://github.com/shalva97/google-auth-decode/releases/download/0.0.19/shared.xcframework.zip"
 let remoteKotlinChecksum = "abb3feeb765f83a1cc27ad48c880f68f9e51dc2598b38be2869dfa53ce61f024"
 let packageName = "com_github_shalva97"
 
