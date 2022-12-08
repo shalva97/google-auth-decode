3c2be7d21124fdd43c66dd5e7c9b9f6d2310c6b6
George Shalvashvili <shalva258@gmail.com>
2022-12-08 20:08:08 +0400

    update checksum

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let remoteKotlinUrl = "https://github.com/shalva97/google-auth-decode/releases/download/0.0.19/shared.xcframework.zip"
-let remoteKotlinChecksum = "abb3feeb765f83a1cc27ad48c880f68f9e51dc2598b38be2869dfa53ce61f024"
+let remoteKotlinChecksum = "59d52e04c78f7a91548efbc61f30ded13bd7a4bf41f55cf24f790d33d010b770"
 let packageName = "com_github_shalva97"
 
 let package = Package(
